0dde16564f8d65dfd3782a88cb787d0eb3bc3ccd
JohnW-07 <Johnwu07@outlook.com>
2025-10-17 16:11:08 -0400

    initial final commit

---
+++ src/game/Asteroid.java
@@ -0,0 +1,59 @@
+package game;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class Asteroid extends Polygon implements RandomGeneration {
+	private static Point [] asteroidShape = { new Point (-5,0), new Point (0,5), new Point (5,0), new Point(0,-5)};
+	public Asteroid (int x, int y) {
+		super(asteroidShape, new Point(x,y), 0 );
+		}
+	public void paint (Graphics brush) {
+		int[] x = new int[asteroidShape.length];
+		int[] y = new int[asteroidShape.length];
+		
+		for (int i = 0; i < this.getPoints().length; i++) {
+			x[i] = (int) this.getPoints()[i].getX();
+			y[i] = (int) this.getPoints()[i].getY();
+		}
+		brush.fillPolygon(x, y, asteroidShape.length);
+	}
+	public Point generation () { // RandomGeneration implementation
+		Random rand = new Random ();
+		double randomNumber1 = rand.nextDouble(1000);
+		double randomNumber2 = rand.nextDouble(1000);
+		return new Point (randomNumber1, randomNumber2);
+	}
+	public Point [] asteroidRandomGeneration() {
+		Point [] asteroidShapeRandomGeneration = { generation(), generation(), generation(), generation() };
+		return asteroidShapeRandomGeneration;
+	}
+
+
+	RandomGeneration randomExample = new RandomGeneration() {
+		public Point generation () {
+			Random rand = new Random ();
+			double randomNumber1 = rand.nextDouble(1000);
+			double randomNumber2 = rand.nextDouble(1000);
+			return new Point (randomNumber1, randomNumber2);
+		}
+		public Point [] asteroidRandomGeneration() {
+				Point [] asteroidShapeRandomGeneration = { generation(), generation(), generation(), generation() };
+				return asteroidShapeRandomGeneration;
+			}
+	};
+	
+	
+	private class Counter{
+		int count;
+		int scoreKeeper;
+		int highScoreKeeper;
+		int streak;
+		
+		public Counter(int count,int scoreKeeper, int highScoreKeeper, int streak ) {
+			this.count=count;
+			this.scoreKeeper=scoreKeeper;
+			this.highScoreKeeper=highScoreKeeper;
+			this.streak=streak;
+		}
+	}
+}
---
+++ src/game/RandomGeneration.java
@@ -0,0 +1,9 @@
+package game;
+
+public interface RandomGeneration {
+	public Point generation ();
+
+
+	public Point[] asteroidRandomGeneration();
+	
+}
