4bbda54177451c2b000ef6396f31adfb58769082
shahaan776-hello <shahaan776@gmail.com>
2025-10-17 18:11:21 -0400

    Add files via upload

--- src/game/RandomGeneration.java
+++ src/game/RandomGeneration.java
@@ -1,9 +1,16 @@
 package game;
 
 public interface RandomGeneration {
+	/**
+	 * This is implemented in Asteroid
+	 * @return returns a point randomly generated 
+	 */
 	public Point generation ();
 
-
+/**
+ * This is also implemented in Asteroid
+ * @return A Point array or randomly generated points
+ */
 	public Point[] asteroidRandomGeneration();
 	
 }
